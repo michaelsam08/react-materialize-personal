634f1abc10c5b3f5f622e9585ace8a297267f04c
MichaelSam <michaelsam08@gmail.com>
2018-10-16 17:58:45 -0500

    improvements

--- src/App.js
+++ src/App.js
@@ -1,102 +1,176 @@
-import React, {Component} from 'react';
-import {Navbar,NavItem,Row, Icon, Input,Carousel,Button}  from 'react-materialize';
-import logo from './logo.svg';
-import './App.css';
-import image1 from './images/cartagena.jpg';
-import image2 from './images/dogwatchinghisownersurf.JPG';
-import image3 from './images/olonfromclif.JPG';
-import image4 from './images/sunset1.JPG';
-import image5 from './images/profphoto.jpg';
-import { Document, Page } from 'react-pdf';
-import junDev from './Docs/junDev.pdf'
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavItem,
+  Row,
+  Icon,
+  Input,
+  Carousel,
+  Button,
+  Slider,
+  Slide
+} from "react-materialize";
+import "./App.css";
+import image1 from "./images/cartagena.jpg";
+import image2 from "./images/dogwatchinghisownersurf.JPG";
+import image3 from "./images/olonfromclif.JPG";
+import image4 from "./images/sunset1.JPG";
+import image5 from "./images/profphoto.jpg";
+import { Document, Page } from "react-pdf";
+import MichaelSamaraDevRes from "./Docs/MichaelSamaraDevRes.pdf";
 // added to import my resume in PDF format
 class App extends Component {
   state = {
-  sunrise: null,
-  sunset: null,
-  numPages: null,
-  pageNumber: 1,
-}
+    AQI: null,
+    city: "",
+    country: "",
+    numPages: null,
+    pageNumber: 1
+  };
 
   onDocumentLoad = ({ numPages }) => {
-     this.setState({ numPages });
-   }
+    this.setState({ numPages });
+  };
 
-handleFormSubmit = (e) => {
-  e.preventDefault();
-  const lat = e.target.children[1].value;
-  const lon = e.target.children[3].value;
-  const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=today`;
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const city = this.state.city;
+    const country = this.state.country;
+    const url = `https://api.openaq.org/v1/cities?country=${country}`;
 
-  fetch(url)
+    fetch(url)
       .then(res => res.json())
-      .then((apiResult) => {
+      .then(apiResult => {
+        const results = apiResult.results;
+        const cityAQI = results.find(result => {
+          return result.city.toLowerCase() === city.toLowerCase();
+        });
         this.setState({
-            sunrise: apiResult.results.sunrise,
-            sunset: apiResult.results.sunset
-        })
-      })
+          AQI: cityAQI && cityAQI.count,
+          city: "",
+          country: ""
+        });
+      });
+  };
 
-}
-
+  onChange(e, type) {
+    e.preventDefault();
+    const stateObj = {};
+    stateObj[type] = e.target.value;
+    this.setState(stateObj);
+  }
 
   render() {
     const { pageNumber, numPages } = this.state;
-//for pdf
+    //for pdf
 
-  return (
+    return (
       <div className="App">
-        <Navbar className='green darken-4'>
-          <NavItem href= {'#resume'}> Resume</NavItem>
-          <NavItem href='components.html'>Components</NavItem>
+        <Navbar className="green darken-4">
+          <NavItem href={"#resume"}> Resume</NavItem>
+          <NavItem href="https://github.com/michaelsam08?tab=repositories">
+            GitHub
+          </NavItem>
         </Navbar>
 
-        <h1>{'Michael Samara'}</h1>
+        <h1>{"Fetch...the Air Quality"}</h1>
         <form onSubmit={this.handleFormSubmit}>
-        <p>Latitude:</p>
-        <input type="text" />
-        <p>Longitude:</p>
-        <input type="text" />
-        <br/>
-        <button type="submit">Submit</button>
+          <p>City:</p>
+          <input
+            value={this.state.city}
+            onChange={e => this.onChange(e, "city")}
+            type="text"
+          />
+          <p>Country (ISO code):</p>
+          <input
+            value={this.state.country}
+            onChange={e => this.onChange(e, "country")}
+            type="text"
+          />
+          <br />
+          <button type="submit">Submit</button>
         </form>
 
-      <h3 className='yellow-text'>Sunrise & Sunset</h3>
-        {
-          this.state.sunrise ? (
-            <div className='Sunrise'>
-              <p>{`Sunrise: ${this.state.sunrise}`}</p>
-              <p>{`Sunset: ${this.state.sunset}`}</p>
-            </div>
-          ): null
-        }
-        <Carousel options={{ fullWidth: true}}
-          fixedItem={<h4 className='white-text'>Michael is a creative and dependable Junior Web Developer. Broadly knowledgeable in React, JavaScript, CSS and HTML as well as the principles and techniques of website construction and maintenance.​ ​An ​effective process-driven student and teacher who creates and implements training for businesses and students. Collaborative team player with the ability to effectively communicate at all organizational and education levels. Exceptional problem solving skills, strong business judgement, and demonstrated experience leading cross-functional teams.
-            </h4>}
-          images={[image1, image2, image3, image4, image5]} >
-        </Carousel>
+        <h3 className="orange-text">AQI Score</h3>
+        {this.state.AQI ? (
+          <div className="Sunrise">
+            <p>{`AQI: ${this.state.AQI}`}</p>
+          </div>
+        ) : null}
+        <Slider>
+          <Slide src={image1} color="blue" title="Cartagena Colombia.">
+            Between 2016 and 2017 I lived in Bogota and Barranquilla Colombia.
+            It is a wonderful place.
+          </Slide>
+          <Slide src={image3} title="Ecuadorian Coast" placement="left">
+            I was able to spend 3 months working remotely there last year.
+          </Slide>
+          <Slide src={image5} title="Michael Samara" placement="right">
+            Enjoys working with Austin Coding Academy.
+          </Slide>
+        </Slider>
+        {/* <Carousel
+          options={{ fullWidth: true }}
+          fixedItem={
+            <h4 className="white-text">
+              Michael is a versatile and quick learning Web Developer. Broadly
+              knowledgeable in React, JavaScript, Express, MongoDB, Node.js, CSS
+              and HTML. An effective process-driven developer who creates and
+              implements applications for businesses and individuals.
+              Collaborative team player with the ability to effectively
+              communicate at all organizational and education levels.
+              Exceptional problem solving skills, strong business judgement, and
+              demonstrated experience contributing to and leading
+              cross-functional teams.
+            </h4>
+          }
+          images={[image1, image2, image3, image4, image5]}
+        /> */}
         <div>
           <h3>Request Contact</h3>
-          <Row>
-            <Input s={6} label="First and Last Name" validate><Icon>account_circle</Icon></Input>
-            <Input s={6} label="Email" validate type="Email"><Icon>Email</Icon></Input>
-            <button type='submit'>Request Contact</button>
-          </Row>
-        </div>
-        <div id= 'resume'>
-         <Document
-           file= {junDev}
-           onLoadSuccess={this.onDocumentLoad}
+          <form
+            action="https://formspree.io/michaelsam08@gmail.com"
+            method="POST"
+          >
+            <Input
+              type="text"
+              name="name"
+              s={6}
+              label="First and Last Name"
+              validate
             >
-           <Page pageNumber={pageNumber} />
-         </Document>
-         <p>Page {pageNumber} of {numPages}</p>
-         <div id='sunrise' className="container"> space for text
-         </div>
-       </div>
+              <Icon>account_circle</Icon>
+            </Input>
+            <Input
+              type="email"
+              s={6}
+              label="Email"
+              validate
+              type="Email"
+              name="_replyto"
+            >
+              <Icon>Email</Icon>
+            </Input>
+            <Button name="name" type="submit" value="send">
+              Request Contact
+            </Button>
+          </form>
+        </div>
+        <div id="resume">
+          <Document
+            file={MichaelSamaraDevRes}
+            onLoadSuccess={this.onDocumentLoad}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+          <div id="sunrise" className="container">
+            {" "}
+          </div>
+        </div>
       </div>
-
-
     );
   }
 }
